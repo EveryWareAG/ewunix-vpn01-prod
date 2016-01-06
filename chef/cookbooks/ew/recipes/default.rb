af73091f36ca35831eb5f3aab423ee9b99ccb5d9
Alexander Skwar <a@skwar.me>
2016-01-06 11:19:13 +0100

    Misc.

--- chef/cookbooks/ew/recipes/default.rb
+++ chef/cookbooks/ew/recipes/default.rb
@@ -15,43 +15,11 @@ include_recipe "openssh"
 # "resolver" Cookbook "ausführen" - erzeugt /etc/resolv.conf
 include_recipe 'resolver'
 
+# "ew::repos" Cookbook "ausführen" - passt Ubuntu Repositories in /etc/apt an.
+include_recipe 'resolver'
+
 #############################################
 # Wichtige grundlegende Basics!
-
-# apt/sources.list Datei entfernen
-file "sources.list Datei entfernen" do
-    path "/etc/apt/sources.list"
-    action :delete
-end # file "sources.list Datei entfernen" do
-
-# Ubuntu Repositories
-dist = node['lsb']['codename']
-apt_repository "Ubuntu Repos: " + dist do
-    name "Ubuntu-Repositories-" + dist
-    uri "http://ubuntu.everyware/" + node['ew'][:aptweek] + "/ubuntu"
-    distribution dist
-    components ["main", "restricted", "multiverse", "universe"]
-    deb_src true
-end # of apt_repository "Ubuntu Repos: " + distribution do
-%w{updates backports security}.each do |dist_subtype|
-    dist = node['lsb']['codename'] + dist_subtype
-    apt_repository "Ubuntu Repos: " + dist do
-        name "Ubuntu-Repositories-" + dist
-        uri "http://ubuntu.everyware/" + node['ew'][:aptweek] + "/ubuntu"
-        distribution dist
-        components ["main", "restricted", "multiverse", "universe"]
-        deb_src true
-    end # of apt_repository "Ubuntu Repos: " + distribution do
-end # of %w{updates backports security}.each do |dist_subtype|
-
-# # "ew" Repository einbinden (u.a. für SysView)
-# apt_repository "ew" do
-#     # deb http://ubuntu.everyware/<%= node['ew'][:aptweek] %>/ew trusty main
-#     uri "http://ubuntu.everyware/" + node['ew'][:aptweek] + "/ew"
-#     distribution node['lsb']['codename']
-#     components ["main"]
-#     key "http://ubuntu.everyware/current/keys/ew-repo_7CF7F04D.key"
-# end # of apt_repository "ew" do
 
 # Disable chef-client (we run chef-zero in this kitchen)
 service "Disable chef-client Service" do
--- chef/cookbooks/ew/recipes/ssh.rb
+++ chef/cookbooks/ew/recipes/ssh.rb
@@ -21,19 +21,4 @@ remote_directory "/root/.ssh" do
     source "default-ew-.ssh"
 end
 
-#############################################
-# Deploy key for gitlab repository
-remote_directory "/root/.ssh" do
-    files_mode '0600'
-    files_owner 'root'
-    files_group 'root'
-    mode '0700'
-    owner 'root'
-    group 'root'
-    source "root-ssh"
-end
-file "/root/.ssh/id_rsa.pub" do
-    mode '0644'
-end
-
 # EOF
