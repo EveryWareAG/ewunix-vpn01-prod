0bf4c6f9f3f8aff911956be80bffda2b4512fac7
Alexander Skwar <a@skwar.me>
2016-01-12 14:19:26 +0100

    Whitespace…

--- chef/cookbooks/vpn-server/metadata.rb
+++ chef/cookbooks/vpn-server/metadata.rb
@@ -1,8 +1,8 @@
-name             'vpn-server'
-maintainer       'EveryWare AG'
-maintainer_email 'alexander.skwar@everyware.ch'
-license          'All rights reserved'
-description      'Installs/Configures OpenVPN Server'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
-depends 'apt',   '~> 2.2'
+name                'vpn-server'
+maintainer          'EveryWare AG'
+maintainer_email    'alexander.skwar@everyware.ch'
+license             'All rights reserved'
+description         'Installs/Configures OpenVPN Server'
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             '0.2.0'
+depends             'apt', '~> 2.2'
