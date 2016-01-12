b363609f0bc6f2731c900ae2bd7b700d39822395
Alexander Skwar <a@skwar.me>
2016-01-12 14:23:56 +0100

    openvpn Cookbook von Supermarket hinzugefügt

--- chef/cookbooks/vpn-server/metadata.rb
+++ chef/cookbooks/vpn-server/metadata.rb
@@ -6,3 +6,4 @@ description         'Installs/Configures OpenVPN Server'
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version             '0.2.0'
 depends             'apt', '~> 2.2'
+depends             'openvpn', '~> 2.1.0'
