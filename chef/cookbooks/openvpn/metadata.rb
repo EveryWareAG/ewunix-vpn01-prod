19e188421a8797cde7ccb0c8460b09026a5bea12
Alexander Skwar <a@skwar.me>
2016-01-12 15:55:17 +0100

    Füge yum und yum-epel Cookbooks hinzu

--- chef/cookbooks/openvpn/metadata.rb
+++ chef/cookbooks/openvpn/metadata.rb
@@ -9,16 +9,23 @@ version           '2.1.0'
 recipe 'openvpn', 'Installs and configures openvpn'
 recipe 'openvpn::users', 'Sets up openvpn cert/configs for users data bag items'
 
+depends 'yum', '~> 3.0'
+depends 'yum-epel'
+
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'redhat'
+supports 'ubuntu'
+
 %w{centos fedora redhat}.each do |platform|
-    supports platform
-    depends 'yum', '~> 3.0'
-    depends 'yum-epel'
+  supports platform
 end
 
 %w{debian ubuntu}.each do |platform|
-    supports platform
-    depends 'apt', '~> 2.2'
+  supports platform
 end
+
 
 attribute 'openvpn/local',
           :display_name => 'OpenVPN Local',
