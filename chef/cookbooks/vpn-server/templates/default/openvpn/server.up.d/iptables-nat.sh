2d93d90570797d8271c57ec6faf0baa076d3ffb8
Alexander Skwar <a@skwar.me>
2016-01-12 16:12:00 +0100

    Im Template gibt es die Variablen cookbook_name und recipe_name nicht.

--- chef/cookbooks/vpn-server/templates/default/openvpn/server.up.d/iptables-nat.sh
+++ chef/cookbooks/vpn-server/templates/default/openvpn/server.up.d/iptables-nat.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-# This file is managed by Chef on node <%= node[:hostname] %> 
-# in <%= cookbook_name %>::<%= recipe_name %>.
+# This file is managed by Chef on node <%= node[:hostname] %>.
 # Manual changes might get lost!
 
 # Set up iptables NAT rules as needed.
-/sbin/iptables -t nat -A POSTROUTING -s <%= node['openvpn']['subnet'] %>/<%= node['openvpn']['netmask'] %> -o <%= node['network']['default_interface'] %> -j MASQUERADE
+/sbin/iptables \
+    -t nat \
+    -A POSTROUTING \
+    -s <%= node['openvpn']['subnet'] %>/<%= node['openvpn']['netmask'] %> \
+    -o <%= node['network']['default_interface'] %> \
+    -j MASQUERADE
 
 # EOF
